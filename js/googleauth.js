78eedfe079a1c8a1590337c00c94f5150978239c
smhsu <shoe.hsuman@gmail.com>
2014-12-24 18:57:45 -0600

    Added token validation

--- js/googleauth.js
+++ js/googleauth.js
@@ -4,6 +4,9 @@ var scope = 'https://www.googleapis.com/auth/calendar';
 
 document.getElementById('authorize-button').onclick = handleAuthClick;
 
+var token = null;
+var valid = false;
+
 function handleClientLoad() {
 	gapi.client.setApiKey(apiKey);
 	//window.setTimeout(checkAuth,1);
@@ -17,7 +20,8 @@ function handleAuthResult(authResult) {
 	var authorizeButton = document.getElementById('authorize-button');
 	if (authResult && !authResult.error) {
 		authorizeButton.style.visibility = 'hidden';
-		//makeApiCall();
+		token = authResult;
+		validateToken();
 	} else {
 		authorizeButton.style.visibility = '';
 		authorizeButton.onclick = handleAuthClick;
@@ -27,4 +31,21 @@ function handleAuthResult(authResult) {
 function handleAuthClick(event) {
 	gapi.auth.authorize({client_id: clientId, scope: scope, immediate: false}, handleAuthResult);
 	return false;
+}
+
+
+function validateToken() {
+	valid = false;
+	if (!token)
+		return;
+	url = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=1/";
+	url += token.access_token;
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", url, true);
+	xmlhttp.onload = function() {
+		response = jQuery.parseJSON(xmlhttp.responseText);
+		if (!response.error && response.audience == clientId) 
+			valid = true;
+	}
+	xmlhttp.send();
 }
