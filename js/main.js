c2a72f4630935bf3eb9062996e2f493f072ee3cb
Silas Hsu <hsu.silas@wustl.edu>
2017-10-15 13:21:14 -0500

    New site!

--- js/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-	$("#login").replaceWith(loginDiv);
-	$("#cal-select").append(defaultSelect);
-	$("#cal-select").append(defaultRefreshBtn);
-	document.getElementById('inputbox').addEventListener('input', pastedTextChanged);
-}
